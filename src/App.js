97b74525c271808cc70ab650963b5610b848cff5
abhishek <abhisrajput6244@gmail.com>
2023-08-09 11:56:48 -0700

    This is my first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Login';
+import { Fetchapi } from './Fetchapi';
+import { About } from './About';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="about" element>
+        <Route path></Route>
+        <Route></Route>
+        <Route></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
